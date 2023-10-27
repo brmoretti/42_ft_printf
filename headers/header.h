cf23faa46506203977bb1037b7325398760b75a9
Bruno Moretti <brmoretti@gmail.com>
2023-10-27 00:29:31 -0300

    beggining

---
+++ headers/header.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmoretti <bmoretti@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/26 23:24:33 by bmoretti          #+#    #+#             */
+/*   Updated: 2023/10/26 23:39:17 by bmoretti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_H
+# define HEADER_H
+
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdlib.h>
+
+void	ft_putchar(char c);
+size_t	ft_strlen(const char *s);
+void	ft_putstr(const char *s);
+
+#endif
