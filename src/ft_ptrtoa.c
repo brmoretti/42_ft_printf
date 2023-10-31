ccede87c2f8d16c4d353b86494b5639396be77a4
Bruno Moretti <bmoretti@c2r4p1.42sp.org.br>
2023-10-31 16:14:08 -0300

    aa

--- src/ft_ptrtoa.c
+++ src/ft_ptrtoa.c
@@ -6,7 +6,7 @@
 /*   By: bmoretti <bmoretti@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 15:52:10 by bmoretti          #+#    #+#             */
-/*   Updated: 2023/10/30 16:13:25 by bmoretti         ###   ########.fr       */
+/*   Updated: 2023/10/31 15:18:16 by bmoretti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,3 +32,6 @@ char	*ft_ptrtoa(void *ptr)
 	free (address_no);
 	return (str);
 }
+
+int main(void)
+
--- src/test_var.c
+++ src/test_var.c
@@ -1,22 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   test_var.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bmoretti <bmoretti@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/26 22:57:47 by bmoretti          #+#    #+#             */
-/*   Updated: 2023/10/31 14:52:47 by bmoretti         ###   ########.fr       */
+/*   Created: 2023/10/31 14:17:59 by bmoretti          #+#    #+#             */
+/*   Updated: 2023/10/31 15:15:24 by bmoretti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
+#include <stdio.h>
 
-int	ft_printf(const char *format, ...)
+void	ft_print_int(int count, ...)
 {
 	va_list	args;
+	int		arg;
 
-	va_start(args, format);
-	
+	va_start(args, count);
+	while (count--)
+	{
+		arg = va_arg(args, int);
+		printf("%d\n", arg);
+	}
 	va_end(args);
+}
+
+int	main(void)
+{
+	ft_print_int(5, 1, 2, 3);
+	return (0);
 }
